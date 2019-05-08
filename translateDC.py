f1ca323fce276be273f80be7cfd1f6da111c6a2f
Jacobo Echeverry <42877141+H4K0@users.noreply.github.com>
2019-05-08 18:30:56 -0500

    Funk-tional

--- translateDC.py
+++ translateDC.py
@@ -4,9 +4,23 @@ Script para traducir la tabla de datos
 entregada por la bolsa al insert de MySQL
 para automatizar la carga
 """
-
-
 import os
+months= {
+             'ene':'01',
+             'feb':'02',
+             'mar':'03',
+             'abr':'04',
+             'may':'05',
+             'jun':'06',
+             'jul':'07',
+             'ago':'08',
+             'sep':'09',
+             'oct':'10',
+             'nov':'11',
+             'dic':'12',
+	        }
+inputFile = "CostOnStack.txt"
+outputFile = "insertChart.sql"
 
 def clear():
 	os.system('clear')
@@ -15,17 +29,27 @@ def cwd():
 	a = os.getcwd()
 	return a
 
+def transMonth(MM):
+	mm = months[MM]
+	return mm
+
+def translate(line):
+	#line = str(line)
+	DD   = line[:2]
+	MM   = line[3:6]
+	YYYY = line[7:11]
+	month = transMonth(MM)
+	date = YYYY + '-' + month + '-' + DD
+	return date
 
 os.chdir(cwd())
-
-inputFile = "CostoBolsa.txt"
-outputFile = "insertChart.sql"
-
 ifstream = open(inputFile, 'r')
 ofstream = open(outputFile, 'w')
 
 for aline in ifstream:
-	print(aline, end='')
+	line = translate(aline)
+	print(line)
+
 
 
 
